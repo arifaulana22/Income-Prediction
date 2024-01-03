93492d076ce38f3bb412a5e2f665ad9c467d93cd
arifaulana22 <73513904+arifaulana22@users.noreply.github.com>
2024-01-03 15:01:18 +0700

    Add files via upload

---
+++ Streamlit/Income.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Income Prediction",
+)
+
+st.markdown("# Welcome to My Streamlit!! 👋")
+
+st.markdown(
+    """
+    Welcome Everyone!
+    
+This dataset is about data income in US per year.
+
+Target is <=50K per year or >50K per year
+
+
+"""
+)
+
